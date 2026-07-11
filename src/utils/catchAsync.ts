eb01756c9b170ce3a95d014042e59ae5b60dc881
Pamel-C223171 <Mohammadpamel12345@gmail.com>
2026-07-11 23:37:39 +0300

    after deployment

--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -1,23 +1,3 @@
-// import { NextFunction, Request, RequestHandler, Response } from "express"
-// import httpStatus from 'http-status';
-
-// export const catchAsync = (fn: RequestHandler) => {
-//     return async (req: Request, res: Response, next: NextFunction) => {
-//         try {
-//             await fn(req, res, next)
-//         } catch (error: any) {
-//             console.log(error)
-
-//             res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-//                 success: false,
-//                 statusCode: httpStatus.INTERNAL_SERVER_ERROR,
-//                 message: error.message,
-//                 error: (error as Error).message
-//             })
-//         }
-//     }
-// }
-
 import { NextFunction, Request, RequestHandler, Response } from "express";
 
 export const catchAsync = (fn: RequestHandler) => {
